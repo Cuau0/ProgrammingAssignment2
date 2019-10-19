17c9e50f3b08baf886cbd80621b7ea225a994026
Cuau0 <56003684+Cuau0@users.noreply.github.com>
2019-10-19 01:11:05 -0500

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,4 +1,10 @@
 
+#Functions were created to store an matrix and then calculate its inverse
+
+#Save and create a special matrix based on the matrix assigned by the user 
+#which is stored in the cache to be used later 
+#with their respective functions in a list of 3 elements
+
 makeCacheMatrix <- function(x = matrix()){
   n <- NULL
   get <- function() x
@@ -9,6 +15,10 @@ makeCacheMatrix <- function(x = matrix()){
        getinv = getinv)
   }
   
+#Solve the saved matrix using the functions of the defined list to 
+#be able to solve the inverse of the matrix using 
+#the "solver" function and solve the inverse directly.
+
 cacheSolve <- function(x,...){
   n <- x$getinv()
   if(!is.null(n)) {
@@ -20,8 +30,6 @@ cacheSolve <- function(x,...){
   x$setinv(n)
   n
 }
- 
-
  
 ##Example
 t <- matrix(c(1,3,3,5,5,7,7,9,9), ncol = 3, nrow = 3)  
